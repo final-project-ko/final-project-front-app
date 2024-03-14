f968adcc222f9d22b4334c45317361aed018a5ad
jmsdew <jmsdew@naver.com>
2024-03-14 09:24:11 +0900

    no message

--- react-app/ifconfig/Inet.js
+++ react-app/ifconfig/Inet.js
@@ -1,12 +1,6 @@
 
 
-<<<<<<< HEAD
-export const homeUrl = "192.168.0.159";
 
-=======
-// export const homeUrl = "172.30.1.49";
-// export const homeUrl = "172.30.1.26";
->>>>>>> cb0ef40efb55c8890c51fcd580f9c312c945b6ee
 
 export const homeUrl = "192.168.0.203";
 
